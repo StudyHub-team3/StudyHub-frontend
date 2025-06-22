8fadfb54acf130425c9da04526cfb5bea4132282
danajlim <danalim0819@gmail.com>
2025-06-22 14:28:52 +0900

    feat: button component 추가

--- src/pages/studies/StudyList.tsx
+++ src/pages/studies/StudyList.tsx
@@ -2,14 +2,18 @@
 // 전체 스터디 목록 페이지
 
 import StudyCard from "@/components/design/StudyCard"
+import Button from "@/components/design/Button"
 import dummyStudyList from "@/data/dummyStudyList"
 import Header from "@/components/design/Header"
 
 export default function StudyList() {
   return (
-  <div className="w-full flex justify-center">  
+    <div className="min-h-screen w-full max-w-screen overflow-x-hidden flex flex-col">
   <Header />
   <div className="pt-6 px-4">
+    <div className="flex justify-center mb-6">
+      <Button onClick={() => alert('버튼 클릭됨')}>스터디 생성</Button>
+    </div>
     <div className="w-full flex flex-wrap justify-center gap-6 mt-6">
       {dummyStudyList.map((study) => (
         <StudyCard
