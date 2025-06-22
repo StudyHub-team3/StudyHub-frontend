6f6df785ad814021a9e001bc5486c2d1dc3026b5
danajlim <danalim0819@gmail.com>
2025-06-22 10:55:15 +0900

    feat: add Pretendard font style 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,28 @@ export default {
       center: true,
       padding: '1rem'
     },
-    extend: {},
+    extend: {
+      colors: {
+        button: "#FFA61E",         // 주황색 버튼
+        "primary-light": "#FFE095", // 연한 노랑 버튼
+        "gray-label": "#6b7280",
+        "gray-caption": "#9ca3af",
+        primary: "#FFCC80",          // 헤더 배경
+        "text-main": "#525252",     // 기본 텍스트
+        input: "#9E9E9E",           // 입력창 배경/텍스트
+        card: "#9E9E9E",            // 스터디카드 배경
+        confirm: "#82B7F5",         // 확인 버튼
+        cancel: "#FF978C",          // 취소 버튼
+        background: "#FAFAFA",      // 전체 배경
+        border: "#D6D6D6",          // 테두리
+        disabled: "#BDBDBD",        // 비활성화 텍스트/요소
+        highlight: "#FFF7E0",       // 강조 배경
+        "hover-primary": "#e68c0f"  // hover 상태용
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
