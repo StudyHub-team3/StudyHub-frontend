77d792ad56169c36ab79dc14fbd510c212433a72
NamSangwon <nsw3265@naver.com>
2025-06-26 01:17:00 +0900

    feat : 로그인 필요 없는 요청 추가

--- src/constants/excludedUrls.ts
+++ src/constants/excludedUrls.ts
@@ -2,4 +2,7 @@ export const EXCLUDED_ROUTES = [
   { method: "POST", path: "/api/users/auth/login" },
   { method: "POST", path: "/api/users/auth/register" },
   { method: "POST", path: "/api/users/auth/refresh" },
+  { method: "GET", path: "/api/studies" },
+  { method: "GET", path: "/api/studies/search" },
+  { method: "GET", path: "/api/studies/:studyId" },
 ];
